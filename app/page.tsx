a827a1cb4e78c0c235293aad06240f0bc24c40e1
RyanHermes <boudyhermes@gmail.com>
2024-08-08 01:11:17 -0400

    chore: Add fonts and utils modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
+import { majorMonoDisplay } from './fonts'
+import { useState, useEffect } from 'react'
+import { AuroraBackground } from '@/components/ui/aurora-background'
+import { TypewriterEffectSmooth } from '@/components/ui/typewriter-effect'
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -25,38 +27,54 @@ export default function Home() {
   return (
     <main>
       <header
-        className={`flex items-center justify-between bg-gray-100 p-4 transition-transform duration-300 ${isScrolled ? '-translate-y-full' : 'translate-y-0'}`}
+        className={`flex items-center justify-between bg-black p-4 transition-transform duration-300 ${isScrolled ? '-translate-y-full' : 'translate-y-0'}`}
       >
         <div className="flex items-center">
-          <Image src="/logo.png" alt="logo" width={100} height={100} />
-          <h1 className="ml-4 text-xl font-bold">My Website</h1>
+          <h1
+            className={`${majorMonoDisplay.className} ml-4 text-xl font-bold`}
+          >
+            Ryan Hermes
+          </h1>
         </div>
         <nav>
           <ul className="m-0 flex list-none p-0">
             <li className="mx-4">
-              <Link href="/" className="text-blue-500 hover:text-blue-700">
-                Home
-              </Link>
+              <button className="duration-400 transform rounded-lg border border-black bg-transparent px-6 py-2 font-bold text-black shadow-[0_0_0_3px_#000000_inset] transition hover:-translate-y-1 dark:border-white dark:text-white">
+                <Link href="/">Home</Link>
+              </button>
             </li>
             <li className="mx-4">
-              <Link href="/about" className="text-blue-500 hover:text-blue-700">
-                About
-              </Link>
+              <button className="duration-400 transform rounded-lg border border-black bg-transparent px-6 py-2 font-bold text-black shadow-[0_0_0_3px_#000000_inset] transition hover:-translate-y-1 dark:border-white dark:text-white">
+                <Link href="/about">About</Link>
+              </button>
             </li>
             <li className="mx-4">
-              <Link
-                href="/contact"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                Contact
-              </Link>
+              <button className="duration-400 transform rounded-lg border border-black bg-transparent px-6 py-2 font-bold text-black shadow-[0_0_0_3px_#000000_inset] transition hover:-translate-y-1 dark:border-white dark:text-white">
+                <Link href="/contact">Contact</Link>
+              </button>
             </li>
           </ul>
         </nav>
       </header>
-      <h1 className="mt-8 text-2xl font-bold">Home Page</h1>
-      <div className="h-screen"></div> {/* Added space for testing scroll */}
-      <div className="h-screen"></div> {/* Added space for testing scroll */}
+      <AuroraBackground
+        children={
+          <div className="relative flex flex-col items-center justify-center gap-4 px-4">
+            <div className="text-center text-3xl font-bold md:text-7xl dark:text-white">
+              Coming Soon.
+            </div>
+            <div className="py-4 text-base font-extralight md:text-4xl dark:text-neutral-200">
+              Coming Soon.
+            </div>
+            <button className="w-fit rounded-full bg-black px-4 py-2 text-white dark:bg-white dark:text-black">
+              Coming Soon.
+            </button>
+            <TypewriterEffectSmooth words={[{ text: 'Coming Soon.' }]} />
+          </div>
+        }
+      />
+      <div className="bg-zinc-900">
+        <div className="h-screen"></div> {/* Added space for testing scroll */}
+      </div>
     </main>
   )
 }
