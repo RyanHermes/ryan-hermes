aa093ec32a58c7d0c643834b9f5f7c6668448a4b
RyanHermes <boudyhermes@gmail.com>
2025-05-23 22:02:11 -0400

    build: update eslint config and add import plugin

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+
 import { majorMonoDisplay } from '../app/fonts'
 import styles from '../styles/hamburgers.module.css'
 
